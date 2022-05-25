7c3cb032c61f8d73b4f1d690cd57e7c8e9b99e5c
Panagiotis Xynos <panagiotis.xynos@sap.com>
2022-05-25 12:17:51 +0100

    cf.sh downloads and installs the stemcell it needs

--- cf.sh
+++ cf.sh
@@ -41,10 +41,20 @@ if [ ! -d "$cf_deployment_path" ]; then
   git clone https://github.com/cloudfoundry/cf-deployment.git "$cf_deployment_path"
 fi
 
+stemcells_path="$script_dir/stemcells"
+stemcell_os=$(bosh interpolate --path=/stemcells/0/os "${cf_deployment_path}/cf-deployment.yml")
+stemcell_version=$(bosh interpolate --path=/stemcells/0/version "${cf_deployment_path}/cf-deployment.yml")
+stemcell_url="https://bosh.io/d/stemcells/bosh-warden-boshlite-${stemcell_os}-go_agent?v=${stemcell_version}"
+stemcell_file="$stemcells_path/bosh-stemcell-${stemcell_version}-warden-boshlite-${stemcell_os}-go_agent.tgz"
+if [ ! -f "$stemcell_file" ]; then
+  wget "${stemcell_url}" --output-document="${stemcell_file}"
+fi
+bosh -e "$BOSH_ENVIRONMENT" -n upload-stemcell "$stemcell_file"
+
 pushd "$cf_deployment_path"
 
 bosh -e "$BOSH_ENVIRONMENT" -n update-cloud-config iaas-support/bosh-lite/cloud-config.yml
-bosh -e $BOSH_ENVIRONMENT -n -d cf deploy cf-deployment.yml \
+bosh -e "$BOSH_ENVIRONMENT" -n -d cf deploy cf-deployment.yml \
   --ops-file=operations/bosh-lite.yml \
   --var=system_domain=bosh-lite.com
 
